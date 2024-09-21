825516a4b90db905a9e4880b21b21fafe66a939d
ashmil19 <ashmilabdulrasheed@gmail.com>
2024-09-21 22:39:03 +0530

    state added

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,39 +1,37 @@
 import Slider from '@mui/material/Slider';
-import { useState } from 'react'
 import Good from '../assets/Good.svg'
 import Loveit from '../assets/Loveit.svg'
 import Neutral from '../assets/Neutral.svg'
 import Worst from '../assets/Worst.svg'
 import Fine from "../assets/It's just fine'.svg"
 
-const Rating = () => {
-    const [rating, setRating] = useState(1)
+const Rating = ({values, setValues}) => {
 
   return (
     <div className='flex flex-col gap-3'>
         <div className='flex justify-between'>
-            <div className={`flex flex-col items-center w-[75px] h-[80px] ${rating !== 1 && 'opacity-30'}`}>
+            <div className={`flex flex-col items-center w-[75px] h-[80px] ${values.rating !== 1 && 'grayscale opacity-30'}`}>
                 <img src={Worst} alt="" className='h-[40px] w-[40px]' />
                 <div className='font-semibold text-sm text-[#105955]'>Worst</div>
             </div>
-            <div className={`flex flex-col items-center w-[75px] h-[80px] ${rating !== 2 && 'opacity-30'}`}>
+            <div className={`flex flex-col items-center w-[75px] h-[80px] ${values.rating !== 2 && 'grayscale opacity-30'}`}>
                 <img src={Fine} alt="" className='h-[40px] w-[40px]' />
                 <div className='font-semibold text-center text-sm text-[#105955]'>Not<br/>Good</div>
             </div>
-            <div className={`flex flex-col items-center w-[75px] h-[80px] ${rating !== 3 && 'opacity-30'}`}>
+            <div className={`flex flex-col items-center w-[75px] h-[80px] ${values.rating !== 3 && 'grayscale opacity-30'}`}>
                 <img src={Neutral} alt="" className='h-[40px] w-[40px]' />
                 <div className='font-semibold text-sm text-[#105955]'>Fine</div>
             </div>
-            <div className={`flex flex-col items-center w-[75px] h-[80px] ${rating !== 4 && 'opacity-30'}`}>
+            <div className={`flex flex-col items-center w-[75px] h-[80px] ${values.rating !== 4 && 'grayscale opacity-30'}`}>
                 <img src={Good} alt="" className='h-[40px] w-[40px]' />
                 <div className='font-semibold text-center text-sm text-[#105955]'>Look<br/>Good</div>
             </div>
-            <div className={`flex flex-col items-center w-[75px] h-[80px] ${rating !== 5 && 'opacity-30'}`}>
+            <div className={`flex flex-col items-center w-[75px] h-[80px] ${values.rating !== 5 && 'grayscale opacity-30'}`}>
                 <img src={Loveit} alt="" className='h-[40px] w-[40px]' />
                 <div className='font-semibold text-center text-sm text-[#105955]'>Very<br/>Good</div>
             </div>
         </div>
-        <Slider min={0} max={5} className="!h-2 !text-[#105955]" value={rating} onChange={(e)=> setRating(e.target.value)} />
+    <Slider min={0} max={5} className="!h-2 !text-[#105955]" value={values.rating} onChange={(e)=> setValues((prev)=> ({...prev,rating: e.target.value}))} />
     </div>
   )
 }
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,45 +1,72 @@
-import TextField from '@mui/material/TextField';
 import Rating from '../components/Rating';
+import Neutral from '../assets/Neutral.svg'
+import { useState } from 'react';
 
 const Feedback = () => {
+    const [values, setValues] = useState({
+        name: "",
+        number: "",
+        email: "",
+        comment: "",
+        rating: 1
+    })
+
   return (
-    <div className='w-screen h-screen bg-[#d5e4f1] flex items-center px-16'>
-        <div className='w-[450px] bg-white px-8 py-6 flex flex-col gap-6 rounded-lg'>
-            <div className='text-[#2071b2]'>Please Provide your Feedback</div>
-            <div className='flex flex-col gap-10'>
-                <div className='flex flex-col gap-6'>
-                    <div className='flex justify-between'>
-                        <div className='flex flex-col w-[180px]'>
-                            <label className='font-semibold text-[#2071b2]'>Name</label>
-                            {/* <TextField id="outlined-size-small" className='' size="small" placeholder='John Doe'/> */}
-                            <input type="text" className='border-[1px] border-gray-500 outline-1 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='John Doe' />
+    <div className='w-screen h-screen bg-[#d5e4f1] flex items-center px-16 gap-10'>
+        <div className='flex '>
+            <div className='w-[450px] bg-white px-8 py-6 flex flex-col gap-6 rounded-xl'>
+                <div className='text-[#2071b2]'>Please Provide your Feedback</div>
+                <div className='flex flex-col gap-10'>
+                    <div className='flex flex-col gap-6'>
+                        <div className='flex justify-between'>
+                            <div className='flex flex-col w-[180px]'>
+                                <label className='font-semibold text-[#2071b2]'>Name</label>
+                                <input type="text" name='name' className='border-[1px] border-gray-500 outline-1 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='John Doe' />
+                            </div>
+                            <div className='flex flex-col w-[180px]'>
+                                <label className='font-semibold text-[#2071b2]'>Contact Number</label>
+                                <input type="text" name='number' className='border-[1px] border-gray-500 outline-1 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='+91 00000 00000' />
+                            </div>
                         </div>
-                        <div className='flex flex-col w-[180px]'>
-                            <label className='font-semibold text-[#2071b2]'>Contact Number</label>
-                            <input type="text" className='border-[1px] border-gray-500 outline-1 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='+91 00000 00000' />
-                            {/* <TextField id="outlined-size-small" size="small" placeholder='John Doe'/> */}
+                        <div className='flex'>
+                            <div className='flex flex-col w-[180px]'>
+                                <label className='font-semibold text-[#2071b2]'>Email Address</label>
+                                <input type="text" name='email' className='border-[1px] border-gray-500 outline-1 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='xyz123@gmail.com'/>
+                            </div>
                         </div>
                     </div>
-                    <div className='flex'>
-                        <div className='flex flex-col w-[180px]'>
-                            <label className='font-semibold text-[#2071b2]'>Email Address</label>
-                            <input type="text" className='border-[1px] border-gray-500 outline-1 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='xyz123@gmail.com'/>
-                            {/* <TextField id="outlined-size-small" size="small" placeholder='John Doe'/> */}
-                        </div>
+                    <div className='flex flex-col gap-5'>
+                        <div className='font-semibold text-[#2071b2]'>Share your experience in scaling</div>
+                        <Rating values={values} setValues={setValues} />
+                    </div>
+                    <div className='flex w-full'>
+                        <textarea name="comment" rows={4} className='w-full border-[1px] border-gray-500 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='Add your comments...'></textarea>
+                    </div>
+                    <div >
+                        <button className='w-full p-3 bg-[#20b2aa] font-semibold text-white rounded-lg'>SUBMIT</button>
                     </div>
                 </div>
-                <div className='flex flex-col gap-5'>
-                    <div className='font-semibold text-[#2071b2]'>Share your experience in scaling</div>
-                    <Rating />
-                </div>
-                <div className='flex w-full'>
-                    <textarea name="" id="" rows={4} className='w-full border-[1px] border-gray-500 outline-none focus:border-gray-900 rounded-xl p-2 text-sm' placeholder='Add your comments...'></textarea>
-                </div>
-                <div >
-                    <button className='w-full p-3 bg-[#20b2aa] font-semibold text-white rounded-lg'>SUBMIT</button>
+            </div>
+
+            <div className='w-[550px] bg-[#d5e4f1] px-8 py-4 flex flex-col gap-6 rounded-2xl justify-start '>
+                <div className='font-extrabold text-2xl text-[#2071b2]'>Submitted Feedbacks</div>
+                <div className='flex flex-col gap-3'>
+                    {[1,2,4].map((items)=>(
+                        <div className='bg-white h-28 rounded-lg flex'>
+                            <div className='w-3/4 flex flex-col justify-center pl-5 gap-2'>
+                                <div className='font-semibold text-[#2071b2]'>Comment from the user</div>
+                                <div className='text-[#2071b2]'>User Name</div>
+                            </div>
+                            <div className='w-1/4 flex flex-col justify-center items-center gap-2'>
+                                <img src={Neutral} alt="" className='h-[50px] w-[50px]' />
+                                <div className='text-xs text-center text-green-600 font-semibold'>Very Good</div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
+        
     </div>
   )
 }
