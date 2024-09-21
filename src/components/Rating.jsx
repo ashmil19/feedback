e689e3db556eddbd6f561ab50f567dec959b30d0
ashmil19 <ashmilabdulrasheed@gmail.com>
2024-09-21 11:32:03 +0530

    rating added

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,40 @@
 import Slider from '@mui/material/Slider';
 import { useState } from 'react'
-import React from 'react'
+import Good from '../assets/Good.svg'
+import Loveit from '../assets/Loveit.svg'
+import Neutral from '../assets/Neutral.svg'
+import Worst from '../assets/Worst.svg'
+import Fine from "../assets/It's just fine'.svg"
 
 const Rating = () => {
-    const [rating, setRating] = React.useState(1)
+    const [rating, setRating] = useState(1)
 
   return (
-    <Slider min={0} max={5} className="!h-2" value={rating} onChange={(e)=> setRating(e.target.value)} />
+    <div className='flex flex-col gap-2'>
+        <div className='flex justify-between'>
+            <div className={`flex flex-col items-end w-[75px] h-[80px] ${rating !== 1 && 'opacity-30'}`}>
+                <img src={Worst} alt="" className='h-[40px] w-[40px]' />
+                <div className='font-semibold'>Worst</div>
+            </div>
+            <div className={`flex flex-col items-end w-[75px] h-[80px] ${rating !== 2 && 'opacity-30'}`}>
+                <img src={Fine} alt="" className='h-[40px] w-[40px]' />
+                <div className='font-semibold text-center'>Not<br/>Good</div>
+            </div>
+            <div className={`flex flex-col items-end w-[75px] h-[80px] ${rating !== 3 && 'opacity-30'}`}>
+                <img src={Neutral} alt="" className='h-[40px] w-[40px]' />
+                <div className='font-semibold'>Fine</div>
+            </div>
+            <div className={`flex flex-col items-end w-[75px] h-[80px] ${rating !== 4 && 'opacity-30'}`}>
+                <img src={Good} alt="" className='h-[40px] w-[40px]' />
+                <div className='font-semibold text-center'>Look<br/>Good</div>
+            </div>
+            <div className={`flex flex-col items-end w-[75px] h-[80px] ${rating !== 5 && 'opacity-30'}`}>
+                <img src={Loveit} alt="" className='h-[40px] w-[40px]' />
+                <div className='font-semibold text-center'>Very<br/>Good</div>
+            </div>
+        </div>
+        <Slider min={0} max={5} className="!h-2" value={rating} onChange={(e)=> setRating(e.target.value)} />
+    </div>
   )
 }
 
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -4,14 +4,15 @@ import Rating from '../components/Rating';
 const Feedback = () => {
   return (
     <div className='w-screen h-screen bg-[#d5e4f1]'>
-        <div className='w-1/3 bg-white px-10'>
+        <div className='w-1/3 bg-white p-8'>
             <div>Please Provide your Feedback</div>
             <div>
                 <div>
-                    <div className='flex gap-20'>
+                    <div className='flex gap-10'>
                         <div className='flex flex-col'>
                             <label>Name</label>
-                            <TextField id="outlined-size-small" className='rounded-md' size="small" placeholder='John Doe'/>
+                            <TextField id="outlined-size-small" className='' size="small" placeholder='John Doe'/>
+                            {/* <input type="text" className='p-2 border-0 border-black  rounded-lg' /> */}
                         </div>
                         <div className='flex flex-col'>
                             <label>Contact Number</label>
